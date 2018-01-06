101c547a662a3bb81b122cf4fa6c81a8499bba55
Suhel Chakraborty <chakraborty.suhel@gmail.com>
2018-01-06 23:27:41 +0530

    Replaced unstable Socket.IO with WS(WebSocket)

--- backend/NodeJS/src/api/helper/realtime.js
+++ backend/NodeJS/src/api/helper/realtime.js
@@ -1,8 +1,9 @@
-import Server from 'socket.io'
+import WebSocket from 'ws'
 import * as URL from 'url'
 import * as Utils from './utils'
 
-let io = null
+let wss = null
+let clients = {}
 
 export const Domains = {
     Post: 'post',
@@ -18,69 +19,60 @@ export const Events = {
     Delete: 'delete'
 }
 
-const emit = (domain, event, data, isVolatile, targetUserId) => {
-    if (!io)
-        return
+export const init = (server) => {
 
-    if (!domain || !event || !data)
-        return
-
-    if (!isVolatile || typeof isVolatile !== 'boolean')
-        isVolatile = false
-
-    if (!targetUserId || typeof targetUserId !== 'string')
-        targetUserId = null
-
-    let IO = io
-
-    if (isVolatile) {
-        console.log('Volatile event')
-        IO = IO.volatile
-    }
-
-    if (targetUserId) {
-        console.log('To specific user')
-        IO = IO.to(targetUserId)
-    }
-
-    console.log('Emitted')
-
-    IO.emit(event, {
-        domain,
-        data
+    wss = new WebSocket.Server({
+        server,
+        path: '/realtime'
     })
+
+    wss.on('connection', processNewConnection)
 }
 
-const generateId = (req) => {
-    console.log(`${req.session.user.name} just connected`)
-    return req.session.user._id
-}
+const processNewConnection = (ws, req) => {
+    const location = URL.parse(req.url, true)
+    const token = location.query.token
 
-const allowRequest = (req, cb) => {
-    const query = URL.parse(req.url, true).query
-    if (!query || !query.token)
-        return cb('Thou shall not pass', false)
+    if (!token || typeof token !== 'string') {
+        ws.terminate()
+        return
+    }
 
-    Utils.checkSessionByToken(query.token, (isValid, session) => {
+    Utils.checkSessionByToken(token, (isValid, session) => {
+        const userId = session.user._id
         if (isValid) {
-            req.session = session
-            cb(null, true)
+            console.log(session.user.name + ' connected')
+            clients[userId] = ws
+            ws.on('close', () => {
+                delete clients[`${userId}`]
+            })
         } else {
-            cb('Thou shall not pass', false)
+            ws.terminate()
         }
     })
 }
 
-export const init = (server) => {
-    io = Server(server, {
-        path: '/realtime',
-        serveClient: false,
-        pingInterval: 10000,
-        pingTimeout: 5000,
-        cookie: false,
-        allowRequest: allowRequest
-    })
-    io.engine.generateId = generateId
+const emit = (domain, event, data, targetUserId) => {
+
+    if (!wss)
+        return
+
+    if (!data || !event)
+        return
+
+    if (!data)
+        data = null
+
+    const message = JSON.stringify({ domain, event, data })
+
+    if (targetUserId && clients[targetUserId]) {
+        clients[targetUserId].send(message)
+    } else {
+        for (const client in clients) {
+            if (client)
+                clients[client].send(message)
+        }
+    }
 }
 
 export const emitNewPost = (postId, userId, userName) => {
@@ -106,4 +98,94 @@ export const emitEditComment = (postId, userId, comment, userName) => {
 export const emitDeleteComment = (postId, userId, commentId, userName) => {
     emit(Domains.Comment, Events.Delete, { postId, userId, userName })
 }
+
+
+// const emit = (domain, event, data, isVolatile, targetUserId) => {
+//     if (!io)
+//         return
+
+//     if (!domain || !event || !data)
+//         return
+
+//     if (!isVolatile || typeof isVolatile !== 'boolean')
+//         isVolatile = false
+
+//     if (!targetUserId || typeof targetUserId !== 'string')
+//         targetUserId = null
+
+//     let IO = io
+
+//     if (isVolatile) {
+//         console.log('Volatile event')
+//         IO = IO.volatile
+//     }
+
+//     if (targetUserId) {
+//         console.log('To specific user')
+//         IO = IO.to(targetUserId)
+//     }
+
+//     console.log('Emitted')
+
+//     IO.emit(event, {
+//         domain,
+//         data
+//     })
+// }
+
+// const generateId = (req) => {
+//     console.log(`${req.session.user.name} just connected`)
+//     return req.session.user._id
+// }
+
+// const allowRequest = (req, cb) => {
+//     const query = URL.parse(req.url, true).query
+//     if (!query || !query.token)
+//         return cb('Thou shall not pass', false)
+
+//     Utils.checkSessionByToken(query.token, (isValid, session) => {
+//         if (isValid) {
+//             req.session = session
+//             cb(null, true)
+//         } else {
+//             cb('Thou shall not pass', false)
+//         }
+//     })
+// }
+
+// export const init = (server) => {
+//     io = Server(server, {
+//         path: '/realtime',
+//         serveClient: false,
+//         pingInterval: 10000,
+//         pingTimeout: 5000,
+//         cookie: false,
+//         allowRequest: allowRequest
+//     })
+//     io.engine.generateId = generateId
+// }
+
+// export const emitNewPost = (postId, userId, userName) => {
+//     emit(Domains.Post, Events.Create, { postId, userId, userName })
+// }
+
+// export const emitLike = (postId, userId, userName) => {
+//     emit(Domains.Like, Events.Create, { postId, userId, userName })
+// }
+
+// export const emitUnlike = (postId, userId, userName) => {
+//     emit(Domains.Like, Events.Delete, { postId, userId, userName })
+// }
+
+// export const emitCreateComment = (postId, userId, commentId, userName, comment) => {
+//     emit(Domains.Comment, Events.Create, { postId, userId, commentId, userName, comment })
+// }
+
+// export const emitEditComment = (postId, userId, comment, userName) => {
+//     emit(Domains.Comment, Events.Update, { postId, userId, comment, userName })
+// }
+
+// export const emitDeleteComment = (postId, userId, commentId, userName) => {
+//     emit(Domains.Comment, Events.Delete, { postId, userId, userName })
+// }
 
--- backend/NodeJS/src/api/index.js
+++ backend/NodeJS/src/api/index.js
@@ -3,7 +3,7 @@ import express from 'express'
 import * as Features from './features'
 
 export function init(app, baseUrl) {
-    app.use(`/${baseUrl}/api/v1`, morgan('combined'))
+    // app.use(`/${baseUrl}/api/v1`, morgan('combined'))
     app.use(`/${baseUrl}/api/v1`, express.urlencoded({ extended: true }))
     app.use(`/${baseUrl}/api/v1`, express.json())
     app.use(`/${baseUrl}/api/v1`, Features.route())
